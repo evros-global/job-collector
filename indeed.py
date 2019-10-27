e32c821fa931a343895d5b3c240a23e66bf85a2a
0robert0 <56759095+0robert0@users.noreply.github.com>
2019-10-27 19:12:56 +0200

    indeed first 100 job title-company name and description scrapper codes and write a csv

--- indeed.py
+++ indeed.py
@@ -1,42 +1,52 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import csv
+url=("https://www.indeed.com/q-Web-Developer-l-San-Francisco,-CA-jobs.html","https://www.indeed.com/jobs?q=Web+Developer&l=San+Francisco%2C+CA&start=10","https://www.indeed.com/jobs?q=Web+Developer&l=San+Francisco%2C+CA&start=20","https://www.indeed.com/jobs?q=Web+Developer&l=San+Francisco%2C+CA&start=30","https://www.indeed.com/jobs?q=Web+Developer&l=San+Francisco%2C+CA&start=40","https://www.indeed.com/jobs?q=Web+Developer&l=San+Francisco%2C+CA&start=50","https://www.indeed.com/jobs?q=Web+Developer&l=San+Francisco%2C+CA&start=60","https://www.indeed.com/jobs?q=Web+Developer&l=San+Francisco%2C+CA&start=70","https://www.indeed.com/jobs?q=Web+Developer&l=San+Francisco%2C+CA&start=80","https://www.indeed.com/jobs?q=Web+Developer&l=San+Francisco%2C+CA&start=90")
 
-url="https://www.indeed.com/q-Web-Developer-l-San-Francisco,-CA-jobs.html"
-r = requests.get(url)
-soup = bs(r.text,"html.parser")
+jobs = []
+comName=[]
+desc=[]
+loca=[]
+date=[]
+
+for x in url:
+
+    r = requests.get(x)
+    soup = bs(r.text,"html.parser")
+
+    def extract_job_title_from_result(soup): 
+        for div in soup.find_all(name="div", attrs={"class":"jobsearch-SerpJobCard unifiedRow row result"}):
+            for span in div.find_all(name="span", attrs={"class":"company"}):
+                a=span.get_text()
+                comName.append(a)
+            for a in div.find_all(name="a", attrs={"data-tn-element":"jobTitle"}):
+                jobs.append(a["title"])
+            for div in div.find_all(name="div", attrs={"class":"summary"}):
+                a=div.get_text()
+                desc.append(a)
+            for span in div.find_all(name="span", attrs={"class":"date"}):
+                a=span.get_text()
+                loca.append(a)
+            for div in div.find_all(name="div", attrs={"class":"jobsearch-SerpJobCard-footer"}):
+                a=div.get_text()
+                date.append(a)
+        return()
+    extract_job_title_from_result(soup)
 
 
-def extract_job_title_from_result(soup): 
-    jobs = []
-    comName=[]
-    desc=[]
-    loca=[]
-    date=[]
-    for div in soup.find_all(name="div", attrs={"class":"jobsearch-SerpJobCard unifiedRow row result"}):
-        for span in div.find_all(name="span", attrs={"class":"company"}):
-            a=span.get_text()
-            comName.append(a)
-        for a in div.find_all(name="a", attrs={"data-tn-element":"jobTitle"}):
-            jobs.append(a["title"])
-        for div in div.find_all(name="div", attrs={"class":"summary"}):
-            a=div.get_text()
-            desc.append(a)
-        for span in div.find_all(name="span", attrs={"class":"date"}):
-            a=span.get_text()
-            loca.append(a)
-        for div in div.find_all(name="div", attrs={"class":"jobsearch-SerpJobCard-footer"}):
-            a=div.get_text()
-            date.append(a)
-    print(jobs)
-    print(comName)
-    print(desc)
-    print(date)
-    
-    with open ("c:/users/admin/desktop/newcsv.csv", "w", newline="") as f:
-        writer =csv.writer(f)
-        writer.writerows(jobs)
-        writer.writerows(comName)
-        writer.writerows(desc)
-    return(jobs)
-extract_job_title_from_result(soup)
+#deleting \n and - I'll use - as delimiter
+def orderlist(liste):
+    a=0
+    for x in liste:
+        liste[a]=liste[a].replace("\n","")
+        liste[a]=liste[a].replace("-"," ")
+        a=a+1
+orderlist(comName)
+orderlist(desc)
+
+#creating csv
+with open ("c:/users/admin/desktop/newcsv.csv", "w", newline="") as f:
+    writer =csv.writer(f, delimiter="-")
+    writer.writerow(jobs)
+    writer.writerow(comName)
+    writer.writerow(desc)
