a1b4ce1144c8075d11c1631137c9984f3f679efb
Isa Pakatci <isakemal@gmail.com>
2020-04-22 19:45:39 -0700

    working  web interface (flask) for indeed app
    also changed indeed.py to be more modular

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, send_file, request
+from indeed import *
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return """
-indeed url: <input></input> <br>
-number of pages url: <input></input> <br>
-<input type=submit></input>
+def main():
+   options ='\n'.join(['<option value="{}">{}</option>'.format(i,i)  for i in range(1,50)])
+     
+   return """
+<form method=POST>
+indeed url: <input name="url" size="100"></input> <br>
+<b>Example url:</b> https://www.indeed.com/jobs?q=social+media&l=United+States&explvl=entry_level&remotejob=1 </br><br>
+number of pages: 
+<select name="num_pages"> """ + options + """
+</select><br><br>
+<input type=submit></input><br>
+Please click submit only once and wait for the file to be downloaded
+</form>
 """
+
+@app.route('/', methods=["POST"])
+def return_file():
+  scrape_indeed(request.form['url'], int(request.form['num_pages']))
+  return send_file("newcsv.csv")
