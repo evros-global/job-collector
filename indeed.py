9ef47d28d79282d0ee63a471dae99587dc863fbd
Burak-Esen <0.burak.esen@gmail.com>
2020-04-25 18:31:54 +0300

    modify creating csv

--- indeed.py
+++ indeed.py
@@ -53,6 +53,6 @@ def scrape_indeed(url, num_pages):
     #creating csv
     with open ("./newcsv.csv", "w", newline="") as f:
         writer =csv.writer(f, delimiter="-")
-        writer.writerow(jobs)
-        writer.writerow(comName)
-        writer.writerow(desc)
+        writer.writerow("Job Title","Company Name","Description")
+        for i in range(len(jobs)):
+            writer.writerow(jobs[i],comName[i],desc[i])
